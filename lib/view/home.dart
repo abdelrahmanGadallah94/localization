2a40bde39bac5faa2e9d55c793caff56fc70f88f
abdelrahmanGadallah94 <abhg94@yahoo.com>
2023-01-28 18:13:41 +0200

    dialog using getx

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,9 +10,36 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [Text("Dialog ")]),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text("Dialog "),
+          ElevatedButton(
+              onPressed: () {
+                Get.defaultDialog(
+                    backgroundColor: Colors.green,
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                    custom: Text("test"),
+                    radius: 50,
+                    middleText: "test",
+                    titleStyle: TextStyle(color: Colors.red),
+                    content: Column(
+                      children: [
+                        TextField(),
+                        TextField(),
+                        ElevatedButton(onPressed: () {}, child: Text("ok"))
+                      ],
+                    )
+                    // cancel: const Icon(Icons.close),
+                    // confirm: Text("confirm"),
+                    // actions: [
+                    //   ElevatedButton(
+                    //       onPressed: () {}, child: const Text("confirm")),
+                    //   ElevatedButton(
+                    //       onPressed: () {}, child: const Text("cancel")),
+                    // ],
+                    );
+              },
+              child: const Text("show"))
+        ]),
       ),
     );
   }
